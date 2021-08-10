25a85864ee25c8a294bb16a2941cbfcaaf5825d5
Michal Zerola <michal.zerola@deutsche-boerse.com>
2021-08-10 17:05:02 +0200

    gh actions

--- create_docker.sh
+++ create_docker.sh
@@ -9,8 +9,8 @@ WHEREAMI=`dirname "${0}"`
 docker build -t ${DOCKER_ORG}/${DOCKER_IMAGE_NAME}:${CIRCLE_BRANCH} ${WHEREAMI}
 docker tag ${DOCKER_ORG}/${DOCKER_IMAGE_NAME}:${CIRCLE_BRANCH} ${DOCKER_HUB}/${DOCKER_ORG}/${DOCKER_IMAGE_NAME}:${CIRCLE_BRANCH}
 
-set +o xtrace
-echo -n "${DOCKER_PASSWORD}"  | docker login -u="${DOCKER_USERNAME}" --password-stdin ${DOCKER_HUB}
-set -o xtrace
-
-docker push ${DOCKER_HUB}/${DOCKER_ORG}/${DOCKER_IMAGE_NAME}:${CIRCLE_BRANCH}
+#set +o xtrace
+#echo -n "${DOCKER_PASSWORD}"  | docker login -u="${DOCKER_USERNAME}" --password-stdin ${DOCKER_HUB}
+#set -o xtrace
+#
+#docker push ${DOCKER_HUB}/${DOCKER_ORG}/${DOCKER_IMAGE_NAME}:${CIRCLE_BRANCH}
